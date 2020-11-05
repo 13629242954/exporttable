ce58b51e6770413d726c313e2d082490c5e592c3
13629242954 <13629242954@163.com>
2020-11-05 16:18:53 +0800

    Initial commit

---
+++ src/main/java/com/zhang/exporttable/ExporttableApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.exporttable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExporttableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExporttableApplication.class, args);
+    }
+
+}
